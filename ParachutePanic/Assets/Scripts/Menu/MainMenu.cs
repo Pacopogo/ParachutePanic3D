88927c527db280028936e1ddaee59d1a99b3a516
Pablo <63043113+Pacopogo@users.noreply.github.com>
2024-10-20 20:38:51 +0200

    AudioSlider Added

--- ParachutePanic/Assets/Scripts/Menu/MainMenu.cs
+++ ParachutePanic/Assets/Scripts/Menu/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using TMPro;
 
 [RequireComponent(typeof(SceneLoader))]
@@ -15,12 +16,18 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Slider sensSlider;
     [SerializeField] private TMP_Text mouseSensText;
 
+    [Header("Audio")]
+    [SerializeField] private Slider audioSlide;
+    [SerializeField]private AudioMixer audioMixer;
+    [SerializeField] private TMP_Text audioText;
+
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         sceneLoader = GetComponent<SceneLoader>();
         sensSlider.value = playerSettings.mouseSensitivity;
-
+        changeGlobalAudio();
     }
 
     public void PlayGame()
@@ -39,6 +46,12 @@ public class MainMenu : MonoBehaviour
     {
         playerSettings.mouseSensitivity = sensSlider.value;
         mouseSensText.text = sensSlider.value.ToString("f1");
+    }
+
+    public void changeGlobalAudio()
+    {
+        audioMixer.SetFloat("game", audioSlide.value);
+        audioText.text = (audioSlide.value + 80).ToString("f0") + "%";
     }
 
     public void ExitGame()
