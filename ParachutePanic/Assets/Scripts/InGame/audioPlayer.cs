7183332fed56a460a2d0dcbfb985fb3eda7ae87d
Pablo Jacobs <pablo.jacobs.02@gmail.com>
2024-10-22 17:27:24 +0200

    into Pauze music

--- ParachutePanic/Assets/Scripts/InGame/audioPlayer.cs
+++ ParachutePanic/Assets/Scripts/InGame/audioPlayer.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class audioPlayer : MonoBehaviour
 {
+    [Header("Voice Lines")]
     [SerializeField] private VoiceLines voiceLines;
     AudioSource audioSource;
+
+    [Header("Background Audio")]
+    [SerializeField] private AudioSource BackgroundPlayer;
+    private bool isBGplaying;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
+        isBGplaying = BackgroundPlayer.isPlaying;
     }
 
     public void playLine(int index)
@@ -25,5 +32,18 @@ public class audioPlayer : MonoBehaviour
         audioSource.clip = voiceLines.line[i];
         audioSource.Play();
         yield return new WaitForEndOfFrame();
+    }
+
+    public void pauseBackgroundMusic()
+    {
+        if (isBGplaying)
+        {
+            BackgroundPlayer.Pause();
+            isBGplaying = false;
+            return;
+        }
+        BackgroundPlayer.UnPause();
+        isBGplaying = true;
+
     }
 }
